80b2f106475db779741cab4c1af4cac3cbd704a8
MateuszGajdowicz <155658500+MateuszGajdowicz@users.noreply.github.com>
2025-01-16 17:04:14 +0100

    server1: dostosowany serwer do klienta Dawida klient1: w sumie to wyjebania ale na razie neich zostanie

--- klient/sketch_jan14b.ino
+++ klient/sketch_jan14b.ino
@@ -1,69 +1,228 @@
-const int buttonPin = 0;  // Przyciski wbudowane na GPIO0 (BOOT)
-const int sequenceLength = 4;  // Długość sekwencji
-String userSequence = "";  // Przechowywana sekwencja
+#include <WiFi.h>
+#include <WiFiUdp.h>
 
-unsigned long buttonPressTime = 0;  // Czas naciśnięcia przycisku
-bool buttonState = false;  // Stan przycisku
-bool lastButtonState = false;  // Ostatni stan przycisku
+// Dane logowania do sieci Wi-Fi
+const char* ssid = "wifi";
+const char* password = "haslo";
+
+// Ustawienia sieciowe
+unsigned int localPort = 1305;
+unsigned int serverPort = 1305;
+IPAddress serverIp(192, 168, 0, 208); // Adres IP serwera
+
+// Ustawienia protokołu
+const int sequenceLength = 4;
+String userSequence = "";
+String serverSequence = "";
+const int buttonPin = 0;
+unsigned long buttonPressTime = 0;
+bool buttonState = false;
+bool lastButtonState = false;
+bool sequenceEntered = false;
+bool isWinner = false; // Flaga informująca o wygranej
+
+// Bufor na odbierane dane
+char packetBuffer[255];
+
+// Obiekt UDP
+WiFiUDP Udp;
+
+// Nagłówki protokołu komunikacyjnego
+#define START 'S'
+#define WINNER 'W'
+#define REGISTER 'R'
+#define GAME 'G'
+#define ACK 'a'
+
+// Id hosta
+int hostId = 1;
 
 void setup() {
-  // Inicjalizacja przycisku BOOT
-  pinMode(buttonPin, INPUT_PULLUP);  // Przycisk na GPIO0 (BOOT)
-  
   // Inicjalizacja komunikacji szeregowej
   Serial.begin(115200);
-  Serial.println("Rozpocznij wpisywanie sekwencji za pomocą przycisku BOOT:");
+
+  // Łączenie z siecią Wi-Fi
+  Serial.print("Łączenie z ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("Połączono z Wi-Fi");
+  Serial.print("Adres IP: ");
+  Serial.println(WiFi.localIP());
+
+  // Start UDP
+  Udp.begin(localPort);
+
+  pinMode(buttonPin, INPUT_PULLUP);
 
   // Proces ustawiania sekwencji
+  Serial.println("Rozpocznij wpisywanie sekwencji za pomocą przycisku BOOT:");
   while (userSequence.length() < sequenceLength) {
-    // Odczytanie stanu przycisku
-    buttonState = digitalRead(buttonPin) == LOW;  // LOW oznacza, że przycisk jest wciśnięty
-    
-    // Jeśli przycisk został naciśnięty, zarejestruj czas naciśnięcia
+    buttonState = digitalRead(buttonPin) == LOW;
     if (buttonState && !lastButtonState) {
-      buttonPressTime = millis();  // Rejestracja czasu wciśnięcia przycisku
+      buttonPressTime = millis();
     }
-    
-    // Jeśli przycisk został zwolniony, sprawdź długość naciśnięcia
     if (!buttonState && lastButtonState) {
-      unsigned long pressDuration = millis() - buttonPressTime;  // Oblicz długość naciśnięcia
-      if (pressDuration < 1000) {  // Krótkie naciśnięcie (mniej niż 1 sekunda)
-        userSequence += "0";  // Dodaj "0" do sekwencji
+      unsigned long pressDuration = millis() - buttonPressTime;
+      if (pressDuration < 500) {
+        userSequence += "0";
         Serial.println("Dodano: 0");
-      } else {  // Długie naciśnięcie (ponad 1 sekunda)
-        userSequence += "1";  // Dodaj "1" do sekwencji
+      } else {
+        userSequence += "1";
         Serial.println("Dodano: 1");
       }
-      
-      // Miganie diodą LED w celu sygnalizacji
       signalInput();
     }
-
-    // Zapisanie ostatniego stanu przycisku
     lastButtonState = buttonState;
   }
 
-  // Po zakończeniu wprowadzania sekwencji, wyświetlamy ją
-  Serial.print("Wprowadzona sekwencja: ");
-  Serial.println(userSequence);
-  Serial.println("Sekwencja została ustawiona.");
+  // Po zakończeniu wprowadzania sekwencji
+  if (userSequence.length() == sequenceLength) {
+    Serial.print("Wprowadzona sekwencja: ");
+    Serial.println(userSequence);
+    Serial.println("Sekwencja została ustawiona.");
+    sequenceEntered = true;
+
+    // Wysyłanie informacji rejestracyjnej do serwera po wprowadzeniu sekwencji
+    sendRegistrationInfo();
+  }
 }
 
 void loop() {
+  // Odbieranie pakietów
+  int packetSize = Udp.parsePacket();
+  if (packetSize) {
+    Serial.print("Odebrano pakiet o rozmiarze ");
+    Serial.println(packetSize);
+    Serial.print("Od ");
+    IPAddress remote = Udp.remoteIP();
+    for (int i = 0; i < 4; i++) {
+      Serial.print(remote[i], DEC);
+      if (i < 3) {
+        Serial.print(".");
+      }
+    }
+    Serial.print(", port ");
+    Serial.println(Udp.remotePort());
 
-Serial.print("Sekwencja: ");
-Serial.println(userSequence);
-delay(5000);
-//logika programu
+    // Odczytanie danych z pakietu
+    Udp.read(packetBuffer, 255);
+    packetBuffer[packetSize] = '\0';
 
+    // Przetwarzanie pakietu
+    processPacket(packetBuffer, remote, Udp.remotePort());
+  }
+
+  if (sequenceEntered && !isWinner) {
+    Serial.print("Sekwencja gracza: ");
+    Serial.println(userSequence);
+    sequenceEntered = false;
+  }
+
+  delay(50);
 }
 
-// Funkcja do migania diodą LED w celu sygnalizacji
+void processPacket(char *packet, IPAddress remoteIP, unsigned int remotePort) {
+  char header = packet[0];
+  char *message = packet + 1;
+
+  Serial.print("Odebrano nagłówek: ");
+  Serial.println(header);
+
+  switch (header) {
+    case START:
+      Serial.println("Odebrano START");
+      sendACK(remoteIP, remotePort, START);
+      break;
+    case WINNER:
+      Serial.println("Odebrano WINNER");
+      if (message[0] == '0') {
+        Serial.println("Przegrałeś!");
+        serverSequence = ""; // Czyść sekwencję serwera po otrzymaniu W0
+      } else if (message[0] == '1') {
+        Serial.println("Wygrałeś!");
+        isWinner = true; // Ustaw flagę wygranej
+      }
+      sendACK(remoteIP, remotePort, WINNER);
+      break;
+    case REGISTER:
+      Serial.println("Odebrano REGISTER");
+      sendACK(remoteIP, remotePort, REGISTER);
+      break;
+    case GAME:
+      Serial.println("Odebrano GAME");
+      if (strlen(message) > 0) {
+        serverSequence += message[0];
+      }
+      Serial.print("Aktualna sekwencja serwera: ");
+      Serial.println(serverSequence);
+      checkSequences(remoteIP, remotePort);
+      sendACK(remoteIP, remotePort, GAME);
+      break;
+    case ACK:
+      Serial.print("Odebrano ACK dla: ");
+      Serial.println(message);
+      break;
+    default:
+      Serial.println("Nieznany nagłówek");
+  }
+}
+
+void sendACK(IPAddress remoteIP, unsigned int remotePort, char header) {
+  char ackMessage[3] = {ACK, header, '\0'};
+  Udp.beginPacket(remoteIP, remotePort);
+  Udp.write((const uint8_t *)ackMessage, 2);
+  Udp.endPacket();
+  Serial.print("Wysłano ACK dla: ");
+  Serial.println(header);
+}
+
+void sendRegistrationInfo() {
+  String message = WiFi.localIP().toString() + "," + localPort + "," + hostId;
+  char messageBuffer[50];
+  message.toCharArray(messageBuffer, sizeof(messageBuffer));
+
+  Udp.beginPacket(serverIp, serverPort);
+  Udp.write(REGISTER);
+  Udp.write((const uint8_t *)messageBuffer, strlen(messageBuffer));
+  Udp.endPacket();
+
+  Serial.print("Wysłano informacje rejestracyjne do serwera: ");
+  Serial.println(message);
+}
+
 void signalInput() {
-  int ledPin = 2;  // Zwykle dioda LED jest podłączona do GPIO2
+  int ledPin = 2;
   pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, HIGH);  // Włącz diodę LED
+  digitalWrite(ledPin, HIGH);
   delay(100);
-  digitalWrite(ledPin, LOW);  // Wyłącz diodę LED
+  digitalWrite(ledPin, LOW);
   delay(100);
+}
+
+void checkSequences(IPAddress remoteIP, unsigned int remotePort) {
+    if (serverSequence.length() >= sequenceLength) {
+        String lastServerChars = serverSequence.substring(serverSequence.length() - sequenceLength);
+        if (lastServerChars.equals(userSequence)) {
+            Serial.println("Sekwencja serwera odpowiada sekwencji gracza!");
+            // W przypadku zgodności sekwencji, wyślij W1
+            sendWinnerMessage(remoteIP, remotePort, '1');
+            serverSequence = ""; // Wyczyść sekwencję serwera
+        }
+    }
+}
+
+void sendWinnerMessage(IPAddress remoteIP, unsigned int remotePort, char result) {
+  char winnerMessage[3] = {WINNER, result, '\0'};
+  Udp.beginPacket(remoteIP, remotePort);
+  Udp.write((const uint8_t *)winnerMessage, 2);
+  Udp.endPacket();
+  Serial.print("Wysłano WINNER ");
+  Serial.println(result);
 }
